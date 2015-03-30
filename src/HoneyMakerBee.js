32b834ddde328505a378d351d0d4a16513ace985
prakal <kalvapal@ualberta.ca>
2015-03-30 15:57:26 -0700

    passes most of the tests

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
 var HoneyMakerBee = function() {
+  this.age = 10;
+  this.job = "make honey";
+  this.color = 'yellow';
+  this.food = 'jelly';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype.eat = function(){};
+
+HoneyMakerBee.prototype.makeHoney = function(){
+  this.honeyPot ++;
+};
+HoneyMakerBee.prototype.giveHoney = function(){
+  this.honeyPot --;
+};
